38ba0e712b91dec1988257d76849b7a044796320
Austin <austin.lyon@gmail.com>
2016-06-22 11:55:11 -0700

    added an html file and server endpoint to handle 404 missing page url endpoints

--- server/server.js
+++ server/server.js
@@ -8,12 +8,19 @@ const useragent = require('express-useragent');
 app.use(express.static(path.join(`${__dirname}/../client`)));
 app.use(useragent.express());
 
+// App will serve up different pages for client v& desktop
 app.get('/', (req, res) => {
   if (res.useragent && res.useragent.isMobile) {
-    res.render('../client/mobile.html');
+    res.render(`${__dirname}/../client/mobile.html`);
   } else if (res.useragent && res.useragent.isDesktop) {
-    res.render('../client/browser.html');
+    res.render(`${__dirname}/../client/browser.html`);
   }
+});
+
+// 404 error on invalid endpoint
+app.get('*', (req, res) => {
+  res.status(404)
+     .render(`${__dirname}/client/404.html`);
 });
 
 app.listen(3000, () => {
