f2aa10d23680fa11d9c5daac7926e4de4a9c7043
anwaukoni <anwaukoni@gmail.com>
2016-06-22 12:36:42 -0700

    socket test for user connection

---
+++ test/test.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const expect = require ('chai').expect,
+      io = require('socket.io-client'),
+      url = 'http://localhost:3000',
+      options = {
+        transports:['websocket'],
+        'force new connection': true
+      },
+      user1 = {
+        'name': 'Maam'
+      }
+
+
+describe("socket connection", ()=>{
+  it('should broadcast a username once connected', (done){
+    const client = io.connect(url, options);
+
+    client.on('connect', (data)=>{
+      client.emit('connection name', user1);
+    });
+  });
+
+});
