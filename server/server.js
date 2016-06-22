86ea8e7b0eef6c856f1d107ac0ceccce5841f5b7
Austin <austin.lyon@gmail.com>
2016-06-22 12:35:10 -0700

    handled the event of disconnecting from a socket connection

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
-'use strict';
+'use strict'; // eslint-disable-line
 const express = require('express');
 const app = express();
+const http = require('http').Server(app); //eslint-disable-line
+const io = require('socket.io')(http);
 const path = require('path');
 const bodyParser = require('body-parser');
 const useragent = require('express-useragent');
 
 app.use(express.static(path.join(`${__dirname}/../client`)));
 app.use(useragent.express());
+
+/* ------------------
+ * --    Routes    --
+ * ------------------ */
 
 // App will serve up different pages for client v& desktop
 app.get('/', (req, res) => {
@@ -16,12 +22,30 @@ app.get('/', (req, res) => {
     res.render(`${__dirname}/../client/browser.html`);
   }
 });
-
 // 404 error on invalid endpoint
 app.get('*', (req, res) => {
   res.status(404)
      .render(`${__dirname}/client/404.html`);
 });
+
+/* ------------------
+ * --   Sockets    --
+ * ------------------ */
+
+io.on('connection', (socket) => {
+  console.log('a user connected:', socket);
+  socket.on('tap', () => {
+    console.log('tap from mobile!');
+    io.emit('tap');
+  });
+  socket.on('disconnect', () => {
+    io.emit('user disconnected');
+  });
+});
+
+/* ------------------
+ * --    Server    --
+ * ------------------ */
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
