1e68287ef4071c1acf5bc5b517e45e489caf964f
Austin <austin.lyon@gmail.com>
2016-06-22 14:33:03 -0700

    fixed server path problems when using sendFile method

--- server/server.js
+++ server/server.js
@@ -16,16 +16,21 @@ app.use(useragent.express());
 
 // App will serve up different pages for client v& desktop
 app.get('/', (req, res) => {
-  if (res.useragent && res.useragent.isMobile) {
-    res.render(`${__dirname}/../client/mobile.html`);
-  } else if (res.useragent && res.useragent.isDesktop) {
-    res.render(`${__dirname}/../client/browser.html`);
+  console.log(`request received`);
+  if (req.useragent && req.useragent.isMobile) {
+    console.log(`we're in the mobile`);
+    // res.render(`${__dirname}/../client/mobile.html`);
+    res.sendFile(path.join(`${__dirname}/../client/mobile.html`));
+  } else if (req.useragent && req.useragent.isDesktop) {
+    console.log(`we're in the browser`);
+    res.sendFile(path.join(`${__dirname}/../client/browser.html`));
+    // res.render(`../client/browser.html`);
   }
 });
 // 404 error on invalid endpoint
 app.get('*', (req, res) => {
   res.status(404)
-     .render(`${__dirname}/client/404.html`);
+     .sendFile(path.join(`${__dirname}/../client/404.html`));
 });
 
 /* ------------------
