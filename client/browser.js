de2e3b2a8976bebc7af710650e68420a03697d52
Matthew McLaughlin <Matthew McLaughlin>
2016-06-29 10:32:50 -0700

    Browser javascript code cleaned up, developer side code separated from library side code, verified as working

--- client/browser.js
+++ client/browser.js
@@ -1,5 +1,6 @@
 // 'use strict';
 const socket = io();
+
 const bodyElement = document.querySelector('body');
 const h6Element = document.querySelector('h6');
 const accelDiv = document.getElementById('accel');
@@ -10,40 +11,43 @@ const aZ = document.getElementById('acceleration-z');
 const alpha = document.getElementById('alpha');
 const beta = document.getElementById('beta');
 const gamma = document.getElementById('gamma');
-
-
 const room = Cookies.get('roomId');
-document.getElementById('nonceContainer').innerHTML = `Enter this into your phone,
-please: ${Cookies.get('nonce')}`;
 
+// Add nonce code to screen for mobile users to enter
+document.getElementById('nonceContainer').innerHTML = `Mobile code: ${Cookies.get('nonce')}`;
+// Use roomId from cookies to create a room
 socket.on('connect', () => {
   h6Element.innerHTML = `Socket connection, in ${room}`;
   socket.emit('createRoom', room);
 });
 
-socket.on('tap', changeBody);
-
-socket.on('acceleration', (accelObject) => {
-  aX.innerHTML = `${accelObject.x}`;
-  aY.innerHTML = `${accelObject.y}`;
-  aZ.innerHTML = `${accelObject.z}`;  
-});
-
-socket.on('gyroscope', (gyroObject) => {
-  alpha.innerHTML = `${gyroObject.alpha}`;
-  beta.innerHTML = `${gyroObject.beta}`;
-  gamma.innerHTML = `${gyroObject.gamma}`;
-
-});
-
-function changeBody() {
+function changeBodyClass() {
   if (bodyElement.classList.contains('class1')) {
     bodyElement.classList.remove('class1');
     bodyElement.classList.add('class2');
-    // bodyElement.innerHTML = 'Project-start works!';
   } else {
     bodyElement.classList.remove('class2');
     bodyElement.classList.add('class1');
-    // bodyElement.innerHTML = 'Hello, EchoLoJS';
   }
 }
+
+function updateAccelerationData(accelerationDataObject) {
+  if (accelerationDataObject.x && accelerationDataObject.y && accelerationDataObject.z) {
+    aX.innerHTML = `${accelerationDataObject.x}`;
+    aY.innerHTML = `${accelerationDataObject.y}`;
+    aZ.innerHTML = `${accelerationDataObject.z}`;
+  }
+}
+
+function updateGyroscopeData(gyroscopeDataObject) {
+  if (gyroscopeDataObject.alpha && gyroscopeDataObject.beta && gyroscopeDataObject.gamma) {
+    alpha.innerHTML = `${gyroscopeDataObject.alpha}`;
+    beta.innerHTML = `${gyroscopeDataObject.beta}`;
+    gamma.innerHTML = `${gyroscopeDataObject.gamma}`;
+  }
+}
+
+// Define socket listeners and callback functions
+socket.on('tap', changeBodyClass);
+socket.on('acceleration', updateAccelerationData);
+socket.on('gyroscope', updateGyroscopeData);
