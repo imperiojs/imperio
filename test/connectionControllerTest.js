059d96033ac7299712d78aefbd79a37a1ba0769b
Michael Blanchard <michael.james.blanchard@gmail.com>
2016-07-06 15:33:00 -0700

    adding mocha to DevDependencies.  Created new test file for connectionController.  updated syntax of nonce check in handleGet to match handlePost.

---
+++ test/connectionControllerTest.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect;
+const connectionController = require('./../lib/server/connectionController.js');
+
+describe('GET requests', () => {
+  it('should work!', () => {
+    expect(true).to.be.true;
+  });
+});
