fccf8958538387aa12e0ff9353381fab54fbae6a
Michael Blanchard <michael.james.blanchard@gmail.com>
2016-07-06 21:26:40 -0700

    set up server.js fixture.  Added supertest, created tests sub-folder inside of test folder. started the connectionControllerTest file.

--- test/connectionControllerTest.js
+++
@@ -1,8 +0,0 @@
-const expect = require('chai').expect;
-const connectionController = require('./../lib/server/connectionController.js');
-
-describe('GET requests', () => {
-  it('should work!', () => {
-    expect(true).to.be.true;
-  });
-});
