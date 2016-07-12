654f02565cfb071c8237a1ef932234966177dc66
Michael Blanchard <michael.james.blanchard@gmail.com>
2016-07-12 16:02:41 -0700

    cleaning up webRTC example before implementing into library

--- webRTCDemo/index.js
+++ webRTCDemo/index.js
@@ -1,42 +1,42 @@
+/* eslint-disable no-console, no-param-reassign, no-shadow, no-alert, strict */
 'use strict';
 
-var os = require('os');
-var nodeStatic = require('node-static');
-var http = require('http');
-var socketIO = require('socket.io');
+const os = require('os');
+const nodeStatic = require('node-static');
+const http = require('http');
+const socketIO = require('socket.io');
 
-var fileServer = new(nodeStatic.Server)();
-var app = http.createServer(function(req, res) {
+const fileServer = new(nodeStatic.Server)();
+const app = http.createServer((req, res) => {
   fileServer.serve(req, res);
 }).listen(8080);
 
-var io = socketIO.listen(app);
-io.sockets.on('connection', function(socket) {
-
+const io = socketIO.listen(app);
+io.sockets.on('connection', socket => {
   // convenience function to log server messages on the client
   function log() {
-    var array = ['Message from server:'];
+    let array = ['Message from server:'];
     array.push.apply(array, arguments);
     socket.emit('log', array);
   }
 
-  socket.on('message', function(message) {
+  socket.on('message', message => {
     log('Client said: ', message);
     // for a real app, would be room-only (not broadcast)
     socket.broadcast.emit('message', message);
   });
 
   socket.on('create or join', function(room) {
-    log('Received request to create or join room ' + room);
+    log(`Received request to create or join room ${room}`);
 
-    var numClients = io.sockets.sockets.length;
-    log('Room ' + room + ' now has ' + numClients + ' client(s)');
+    const numClients = io.sockets.sockets.length;
+    log(`Room ${room} now has ${numClients} client(s)`);
 
     if (numClients === 1) {
       socket.join(room);
       log('Client ID ' + socket.id + ' created room ' + room);
       socket.emit('created', room, socket.id);
-    } else if (numClients <= 4) {
+    } else if (numClients === 2) {
       log('Client ID ' + socket.id + ' joined room ' + room);
       // io.sockets.in(room).emit('join', room);
       socket.join(room);
@@ -48,10 +48,10 @@ io.sockets.on('connection', function(socket) {
     }
   });
 
-  socket.on('ipaddr', function() {
-    var ifaces = os.networkInterfaces();
+  socket.on('ipaddr', () => {
+    const ifaces = os.networkInterfaces();
     for (var dev in ifaces) {
-      ifaces[dev].forEach(function(details) {
+      ifaces[dev].forEach(details => {
         if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
           socket.emit('ipaddr', details.address);
         }
@@ -59,8 +59,7 @@ io.sockets.on('connection', function(socket) {
     }
   });
 
-  socket.on('bye', function(){
+  socket.on('bye', () => {
     console.log('received bye');
   });
-
 });
